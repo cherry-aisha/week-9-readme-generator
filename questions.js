d1a7cb7189c35a6055e2e59821a540002285cbbc
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2023-02-05 01:23:57 +0000

    linked createreadme.js

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const createReadMe = require('./createreadme');
+const createReadMe = require('./createreadme.js');
 
 inquirer
     .prompt([
